55f029ec5e0cb9d0f21addd592767f18d55de139
undercovernyan <79418279+undercovernyan@users.noreply.github.com>
2021-02-21 14:05:55 -0500

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ var theme = "frog";
 
 //set stylesheet to theme
 var link = document.getElementById("themeSheet");
-link.setAttribute("href", "frog.css");
+link.setAttribute("href", "themes/" + theme + "css");
 
 //set attribution in footer
 switch (theme) {
