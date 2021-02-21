06d0bef7669263b208d2f6d8adddf4aff1a9da8d
undercovernyan <79418279+undercovernyan@users.noreply.github.com>
2021-02-21 14:13:41 -0500

    Update script.js

--- script.js
+++ script.js
@@ -3,11 +3,11 @@ var theme = "frog";
 
 //set stylesheet to theme
 var link = document.getElementById("themeSheet");
-link.setAttribute("href", "themes/" + theme + "css");
+link.setAttribute("href", "themes/" + theme + ".css");
 
 //set attribution in footer
 switch (theme) {
   case "frog":
-    document.footer.span.innerHTML = ", cursor by <a href='http://www.rw-designer.com/user/90488'>dingdongdie</a>"
+    document.getElementById("attrib").innerHTML = ", cursor by <a href='http://www.rw-designer.com/user/90488'>dingdongdie</a>"
     break;
 }
