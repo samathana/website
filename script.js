f5873a252a28e30f056f4f43f26405371cb5839b
undercovernyan <79418279+undercovernyan@users.noreply.github.com>
2021-02-22 12:53:02 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+//load JS after the page is loaded, to avoid issues with selecting elements that don't exist
+window.onload = function () {
+  
 //set theme to frog by default
 var theme = "frog";
 
@@ -10,4 +13,7 @@ switch (theme) {
   case "frog":
     document.getElementById("attrib").innerHTML = ", cursor by <a href='http://www.rw-designer.com/user/90488'>dingdongdie</a>"
     break;
+}
+
+//end onload function
 }
