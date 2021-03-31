0a15bbedebd2892987636c1dd0f445333cd751e7
undercovernyan <79418279+undercovernyan@users.noreply.github.com>
2021-03-30 23:52:16 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,36 +1,37 @@
 //set theme to frog by default
-var theme = "frog";
+setTheme("frog");
 
 //load JS after the page is loaded, to avoid issues with selecting elements that don't exist
 window.onload = function () {
  
-//change theme when theme buttons are clicked
-var frogBtn = document.getElementById("frogBtn");
-var sheepBtn = document.getElementById("sheepBtn");
-frogBtn.onclick = function() {
- theme = "frog";
-};
-sheepBtn.onclick = function() {
- theme = "sheep";
+  //change theme when theme buttons are clicked
+  var frogBtn = document.getElementById("frogBtn");
+  var sheepBtn = document.getElementById("sheepBtn");
+  frogBtn.onclick = function() {
+   setTheme("frog");
+  };
+  sheepBtn.onclick = function() {
+   setTheme("sheep");
+  };
+  //end onload function
 };
 
-//set stylesheet to theme
-var link = document.getElementById("themeSheet");
-link.setAttribute("href", "themes/" + theme + ".css");
-//avoid cursor attribution on mobile (where cursors don't exist)
-if( /Android|webOS|iPhone|iPad|Mac|Macintosh|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
- theme = "mobile";
-};
+function setTheme(theme) {
+  //set stylesheet to theme
+  var link = document.getElementById("themeSheet");
+  link.setAttribute("href", "themes/" + theme + ".css");
+  //avoid cursor attribution on mobile (where cursors don't exist)
+  if( /Android|webOS|iPhone|iPad|Mac|Macintosh|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+  theme = "mobile";
+  };
   
-//set attribution in footer
-switch (theme) {
-  case "mobile":
-    document.getElementById("attrib").innerHTML = ", but it's better on desktop"
-    break;
-  case "frog":
-    document.getElementById("attrib").innerHTML = ", cursor by <a href='http://www.rw-designer.com/user/90488'>dingdongdie</a>"
-    break;
-};
-
-//end onload function
+  //set attribution in footer
+  switch (theme) {
+    case "mobile":
+     document.getElementById("attrib").innerHTML = ", but it's better on desktop"
+      break;
+    case "frog":
+      document.getElementById("attrib").innerHTML = ", cursor by <a href='http://www.rw-designer.com/user/90488'>dingdongdie</a>"
+      break;
+  };
 };
