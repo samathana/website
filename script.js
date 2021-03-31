82aa3d54a08628423d62de89345a021024a5bc2c
undercovernyan <79418279+undercovernyan@users.noreply.github.com>
2021-03-31 00:25:35 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 //load JS after the page is loaded, to avoid issues with selecting elements that don't exist
 window.onload = function () {
-  
+
   //set the theme according to cookie if it exists
-  if (document.cookie !== "") {
-    setTheme(document.cookie);
-  } else {
+  if (document.cookie == "") {
     setTheme("frog");
+  } else {
+    setTheme(document.cookie);
   };
  
   //change theme when theme buttons are clicked
