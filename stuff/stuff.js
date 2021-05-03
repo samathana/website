44127e325832a05816218e39c3f621d0505c7bc8
undercovernyan <79418279+undercovernyan@users.noreply.github.com>
2021-05-02 23:59:43 -0400

    Update stuff.js

--- stuff/stuff.js
+++ stuff/stuff.js
@@ -1,1 +1,17 @@
+function open(imgNum) {
+  document.getElementById("openImg").style.display = "block";
+  var imgPath = "proof/" + imgNum + ".jpeg";
+  document.getElementById("theImg").setAttribute(src, imgPath);
+}
 
+// Close the Modal
+function close(imgNum) {
+  document.getElementById("openImg").style.display = "none";
+}
+
+// Next/previous controls
+function changeImg(n) {
+  imgNum = imgNum + 1;
+  var imgPath = "proof/" + imgNum + ".jpeg";
+  document.getElementById("theImg").setAttribute(src, imgPath);
+}
