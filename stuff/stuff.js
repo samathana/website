ca86e738452c28932f000fa70dac5d3a21e74ee8
undercovernyan <79418279+undercovernyan@users.noreply.github.com>
2021-05-03 12:29:30 -0400

    Update stuff.js

--- stuff/stuff.js
+++ stuff/stuff.js
@@ -1,15 +1,15 @@
 function open() {
   console.log("er");
-}
+};
 
 // Close the Modal
 function close(imgNum) {
   document.getElementById("openImg").style.display = "none";
-}
+};
 
 // Next/previous controls
 function changeImg(n) {
   imgNum = imgNum + 1;
   var imgPath = "proof/" + imgNum + ".jpeg";
   document.getElementById("theImg").setAttribute(src, imgPath);
-}
+};
