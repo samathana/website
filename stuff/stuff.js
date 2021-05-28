8aeaaa2c5257da76a3cf0a2d1e4f01a647b371ef
undercovernyan <79418279+undercovernyan@users.noreply.github.com>
2021-05-28 16:01:29 -0400

    Update stuff.js

--- stuff/stuff.js
+++ stuff/stuff.js
@@ -2,22 +2,26 @@ function openImg(imgNum) {
   document.getElementById("openImg").style.display = "block";
   var imgPath = "proof/" + imgNum + ".jpeg";
   document.getElementById("theImg").setAttribute("src", imgPath);
+  var aroClicked = 0;
 };
 
 // Close the Modal
 function closeImg(event) {
-  event.stopPropagation();
-  document.getElementById("openImg").style.display = "none";
+  if (aroClicked == 0) {
+    document.getElementById("openImg").style.display = "none";
+  }
 };
 
 // Next/previous controls
 function changeImg(n) {
+  aroClicked = 1;
   //find current img num
-  imgNum = document.getElementById("theImg").src;
+  var imgNum = document.getElementById("theImg").src;
   imgNum = imgNum.split("/")[1];
   imgNum = imgNum.split(".")[0];
   //add n (add or subtract 1)
   imgNum = imgNum + n;
   var imgPath = "proof/" + imgNum + ".jpeg";
   document.getElementById("theImg").setAttribute("src", imgPath);
+  aroClicked = 0;
 };
