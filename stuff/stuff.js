af6c06c2513cf11ddc6f89edb2e0ee78631b4ad0
undercovernyan <79418279+undercovernyan@users.noreply.github.com>
2021-05-29 16:37:25 -0400

    Update stuff.js

--- stuff/stuff.js
+++ stuff/stuff.js
@@ -5,23 +5,23 @@ function openImg(imgNum) {
   var aroClicked = 0;
 };
 
-// Close the Modal
-function closeImg(event) {
-  if (aroClicked == 0) {
-    document.getElementById("openImg").style.display = "none";
-  }
-};
-
 // Next/previous controls
 function changeImg(n) {
   aroClicked = 1;
   //find current img num
   var imgNum = document.getElementById("theImg").src;
-  imgNum = imgNum.split("/")[1];
+  imgNum = imgNum.split("/")[5];
   imgNum = imgNum.split(".")[0];
   //add n (add or subtract 1)
   imgNum = imgNum + n;
   var imgPath = "proof/" + imgNum + ".jpeg";
   document.getElementById("theImg").setAttribute("src", imgPath);
   aroClicked = 0;
+};
+
+// Close modal
+function closeImg(event) {
+  if (aroClicked == 0) {
+    document.getElementById("openImg").style.display = "none";
+  }
 };
