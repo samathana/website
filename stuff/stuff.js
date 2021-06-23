96dafee4dd49362e073024a40cc8949e86973bfd
undercovernyan <79418279+undercovernyan@users.noreply.github.com>
2021-06-22 21:45:50 -0400

    Update stuff.js

--- stuff/stuff.js
+++ stuff/stuff.js
@@ -1,25 +1,26 @@
 function openImg(imgNum) {
   document.getElementById("openImg").style.display = "block";
+  document.getElementById("next").style.display = "block";
+  document.getElementById("prev").style.display = "block";
   var imgPath = "proof/" + imgNum + ".jpeg";
   document.getElementById("theImg").setAttribute("src", imgPath);
 };
 
 // Next/previous controls
-function change(e, direc) {
-    e = window.event || e; 
-    if(this === e.target) {
-        //find current img num
-        var imgNum = document.getElementById("theImg").src;
-        imgNum = imgNum.split("/")[5];
-        imgNum = imgNum.split(".")[0];
-        //add n (add or subtract 1)
-        imgNum = imgNum + direc;
-        var imgPath = "proof/" + imgNum + ".jpeg";
-        document.getElementById("theImg").setAttribute("src", imgPath);
-    }
+function change(n) {
+  //find current img num
+  var imgNum = document.getElementById("theImg").src;
+  imgNum = imgNum.split("/")[5];
+  imgNum = imgNum.split(".")[0];
+  //add n (add or subtract 1)
+  imgNum = imgNum + direc;
+  var imgPath = "proof/" + imgNum + ".jpeg";
+  document.getElementById("theImg").setAttribute("src", imgPath);
 };
 
 // Close modal
 function closeImg() {
     document.getElementById("openImg").style.display = "none";
+    document.getElementById("prev").style.display = "block";
+    document.getElementById("next").style.display = "block";
 };
